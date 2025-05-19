4babdd3af2386b263d75436fdb89d66d744461cf
Codfisher <pony2140@livemail.tw>
2025-05-19 17:50:00 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { useMeta } from 'quasar'
 
-useMeta(() => ({
-  title: `BGM Vue 挑戰`,
-}))
 </script>
 
 <style lang="sass">
--- src/composables/use-lang.ts
+++
@@ -1,7 +0,0 @@
-import type { LangCode, MessageSchema } from '../locales/i18n'
-
-import { useI18n } from 'vue-i18n'
-
-export function useLang() {
-  return useI18n<{ message: MessageSchema }, `${LangCode}`>()
-}
